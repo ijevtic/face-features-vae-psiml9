5f1b529d1537c28ac55003ef2d827c0428d022a1
mihailot01 <mihailo.t.001@gmail.com>
2023-07-28 18:44:14 +0200

    naocare kidaju

--- CustomDataset.py
+++ CustomDataset.py
@@ -4,11 +4,12 @@ import torch
 from PIL import Image
 
 class CustomDataset(Dataset):
-    def __init__(self, img_dir, img_labels, transform=None, target_transform=None):
+    def __init__(self, img_dir, img_labels, transform=None, target_transform=None, loadToRam=False):
         self.img_dir = img_dir
         self.img_labels = img_labels
         self.transform = transform
         self.target_transform = target_transform
+        self.loadToRam = loadToRam
 
         self.imgs = []
         for i in range(len(img_labels)):
@@ -16,14 +17,23 @@ class CustomDataset(Dataset):
             if i%1000 == 0:
                 print(i)
             #print(img_label)
-            img_path = f'{self.img_dir}/{img_label}'
-            image =  Image.open(img_path)
-            if self.transform:
-                image = self.transform(image)
-            self.imgs.append(image)
+            if loadToRam:
+                img_path = f'{self.img_dir}/{img_label}'
+                image =  Image.open(img_path)
+                if self.transform:
+                    image = self.transform(image)
+                self.imgs.append(image)
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        return self.imgs[idx]
+        if self.loadToRam:
+            return self.imgs[idx]
+        else:
+            img_label = self.img_labels[idx]
+            img_path = f'{self.img_dir}/{img_label}'
+            image =  Image.open(img_path)
+            if self.transform:
+                image = self.transform(image)
+            return image
